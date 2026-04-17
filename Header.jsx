7fbef3bb4d4a73cd1fb9d3fb9245ad56ad50d23f
genielson-dev <genielsonlopes241@gmail.com>
2026-04-17 08:13:47 -0300

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header style={{ padding: '20px', borderBottom: '1px solid #444' }}>
+      <h1>Meu Portfólio em React</h1>
+      <p>Desenvolvedor Full Stack em formação</p>
+    </header>
+  )
+}
+
+export default Header
